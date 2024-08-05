7d61695f2aff4c80c5c37608b935021f793e311b
NICOLASR44 <158582207+NICOLASR44@users.noreply.github.com>
2024-08-05 16:20:29 +0200

    Repositoy Program

--- server/app/controllers/categoryActions.js
+++ server/app/controllers/categoryActions.js
@@ -1,17 +1,14 @@
+const tables = require("../../database/tables");
+
 const categories = [
   { id: 1, name: "Science-Fiction" },
   { id: 2, name: "Comédie" },
 ];
 
-const browse = (req, res) => {
-  if (req.query.q != null) {
-    const filteredCategories = categories.filter((category) =>
-      category.name.includes(req.query.q)
-    );
-    res.json(filteredCategories);
-  } else {
-    res.json(categories);
-  }
+const browse = async (req, res) => {
+  const categoriesFromDB = await tables.category.readAll();
+
+  res.json(categoriesFromDB);
 };
 
 const read = (req, res) => {
