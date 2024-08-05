52b5034c99c2cde6e9c2434acea4cbe576b3ab8b
NICOLASR44 <158582207+NICOLASR44@users.noreply.github.com>
2024-08-05 14:20:04 +0200

    add database

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,18 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+create table category (
+  id int unsigned primary key auto_increment not null,
+  name varchar(255) not null
+);
+
+CREATE TABLE program (
+  id  int unsigned primary key auto_increment not null,
+  title varchar(255) not null,
+  synopsis TEXT not null,        
+  poster varchar(255) not null,  
+  country varchar(100) not null, 
+  `year` int not null,            
+  category_id  int unsigned not null
+);
